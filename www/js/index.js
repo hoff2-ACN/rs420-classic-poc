079294e9d771990fbd2b053928d03fad1f8f44fe
Charles Hoffman <charles.d.hoffman@accenture.com>
2019-12-12 11:02:13 -0600

    Add another button

--- www/js/index.js
+++ www/js/index.js
@@ -14,17 +14,46 @@
 
 /* global mainPage, deviceList, refreshButton, statusDiv */
 /* global detailPage, resultDiv, messageInput, sendButton, disconnectButton */
+/* global readButton */
 /* global cordova, bluetoothSerial  */
 /* jshint browser: true , devel: true*/
 'use strict';
 
+function initializeStateMachine() {
+    return new StateMachine({
+        init: 'solid',
+        transitions: [
+            {name: 'melt', from: 'solid', to: 'liquid'},
+            {name: 'freeze', from: 'liquid', to: 'solid'},
+            {name: 'vaporize', from: 'liquid', to: 'gas'},
+            {name: 'condense', from: 'gas', to: 'liquid'}
+        ],
+        methods: {
+            onMelt: function () {
+                console.log('I melted')
+            },
+            onFreeze: function () {
+                console.log('I froze')
+            },
+            onVaporize: function () {
+                console.log('I vaporized')
+            },
+            onCondense: function () {
+                console.log('I condensed')
+            }
+        }
+    });
+}
+
 var app = {
-    initialize: function() {
+    initialize: function () {
         this.bindEvents();
         this.showMainPage();
+        this.machine = initializeStateMachine();
+        console.log("FSM Current State: " + this.machine.state);
     },
-    bindEvents: function() {
 
+    bindEvents: function () {
         var TOUCH_START = 'touchstart';
         if (window.navigator.msPointerEnabled) { // windows phone
             TOUCH_START = 'MSPointerDown';
@@ -34,21 +63,25 @@ var app = {
         sendButton.addEventListener(TOUCH_START, this.sendData, false);
         disconnectButton.addEventListener(TOUCH_START, this.disconnect, false);
         deviceList.addEventListener('touchstart', this.connect, false);
+        readButton.addEventListener(TOUCH_START, this.handleReadButton, false);
     },
-    onDeviceReady: function() {
+
+    onDeviceReady: function () {
         app.refreshDeviceList();
     },
-    refreshDeviceList: function() {
+
+    refreshDeviceList: function () {
         bluetoothSerial.list(app.onDeviceList, app.onError);
     },
-    onDeviceList: function(devices) {
+
+    onDeviceList: function (devices) {
         var option;
 
         // remove existing devices
         deviceList.innerHTML = "";
         app.setStatus("");
 
-        devices.forEach(function(device) {
+        devices.forEach(function (device) {
 
             var listItem = document.createElement('li'),
                 html = '<b>' + device.name + '</b><br/>' + device.id;
@@ -56,15 +89,15 @@ var app = {
             listItem.innerHTML = html;
 
             if (cordova.platformId === 'windowsphone') {
-              // This is a temporary hack until I get the list tap working
-              var button = document.createElement('button');
-              button.innerHTML = "Connect";
-              button.addEventListener('click', app.connect, false);
-              button.dataset = {};
-              button.dataset.deviceId = device.id;
-              listItem.appendChild(button);
+                // This is a temporary hack until I get the list tap working
+                var button = document.createElement('button');
+                button.innerHTML = "Connect";
+                button.addEventListener('click', app.connect, false);
+                button.dataset = {};
+                button.dataset.deviceId = device.id;
+                listItem.appendChild(button);
             } else {
-              listItem.dataset.deviceId = device.id;
+                listItem.dataset.deviceId = device.id;
             }
             deviceList.appendChild(listItem);
         });
@@ -84,17 +117,17 @@ var app = {
         } else {
             app.setStatus("Found " + devices.length + " device" + (devices.length === 1 ? "." : "s."));
         }
-
     },
-    connect: function(e) {
-        var onConnect = function() {
-                // subscribe for incoming data
-                bluetoothSerial.subscribe('\n', app.onData, app.onError);
 
-                resultDiv.innerHTML = "";
-                app.setStatus("Connected");
-                app.showDetailPage();
-            };
+    connect: function (e) {
+        var onConnect = function () {
+            // subscribe for incoming data
+            bluetoothSerial.subscribe('\n', app.onData, app.onError);
+
+            resultDiv.innerHTML = "";
+            app.setStatus("Connected");
+            app.showDetailPage();
+        };
 
         var deviceId = e.target.dataset.deviceId;
         if (!deviceId) { // try the parent
@@ -103,38 +136,43 @@ var app = {
 
         bluetoothSerial.connect(deviceId, onConnect, app.onError);
     },
-    onData: function(data) { // data received from Arduino
+
+    handleReadButton: function (event) {
+        console.log('Hello From The Read Button');
+    },
+
+    onData: function (data) { // data received from Arduino
         console.log(data);
         resultDiv.innerHTML = resultDiv.innerHTML + "Received: " + data + "<br/>";
         resultDiv.scrollTop = resultDiv.scrollHeight;
     },
-    sendData: function(event) { // send data to Arduino
+    sendData: function (event) { // send data to Arduino
 
-        var success = function() {
+        var success = function () {
             console.log("success");
             resultDiv.innerHTML = resultDiv.innerHTML + "Sent: " + messageInput.value + "<br/>";
             resultDiv.scrollTop = resultDiv.scrollHeight;
         };
 
-        var failure = function() {
+        var failure = function () {
             alert("Failed writing data to Bluetooth peripheral");
         };
 
         var data = messageInput.value;
         bluetoothSerial.write(data + "\r\n", success, failure);
     },
-    disconnect: function(event) {
+    disconnect: function (event) {
         bluetoothSerial.disconnect(app.showMainPage, app.onError);
     },
-    showMainPage: function() {
+    showMainPage: function () {
         mainPage.style.display = "";
         detailPage.style.display = "none";
     },
-    showDetailPage: function() {
+    showDetailPage: function () {
         mainPage.style.display = "none";
         detailPage.style.display = "";
     },
-    setStatus: function(message) {
+    setStatus: function (message) {
         console.log(message);
 
         window.clearTimeout(app.statusTimeout);
@@ -146,7 +184,7 @@ var app = {
             statusDiv.className = 'fadeout';
         }, 5000);
     },
-    onError: function(reason) {
+    onError: function (reason) {
         alert("ERROR: " + reason); // real apps should use notification.alert
     }
 };
